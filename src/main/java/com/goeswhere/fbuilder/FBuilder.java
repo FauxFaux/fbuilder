8d26078ad8e8153f656cfa9dcf1e750c30e4acab
Chris West (Faux) <git@goeswhere.com>
2015-10-17 16:35:53 +0100

    sort input so outputs come out in the right order

--- src/main/java/com/goeswhere/fbuilder/FBuilder.java
+++ src/main/java/com/goeswhere/fbuilder/FBuilder.java
@@ -1,11 +1,31 @@
 package com.goeswhere.fbuilder;
 
+import com.google.common.io.Files;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class FBuilder {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        if (args[0].startsWith("@")) {
+            build(Files.readLines(new File(args[0].substring(1)), StandardCharsets.UTF_8));
+        } else {
+            build(Arrays.asList(args));
+        }
+    }
+
+    private static void build(Iterable<String> in) throws IOException, InterruptedException {
+        final List<String> args = newArrayList(in);
+        Collections.sort(args);
+
         final int threads = Runtime.getRuntime().availableProcessors();
 
         final ExecutorService ex = Executors.newFixedThreadPool(threads);
