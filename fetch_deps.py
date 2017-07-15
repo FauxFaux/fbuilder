95f8036cf90b04207c6953e8dad3faad67e93c55
Chris West (Faux) <git@goeswhere.com>
2017-07-15 13:40:50 +0100

    find all the control file entries (commented downloading)

--- fetch_deps.py
+++ fetch_deps.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+import gzip
 import os
 import re
+import subprocess
+import tarfile
 
 import sys
+from collections import defaultdict
 
 
 def main():
@@ -21,13 +25,28 @@ def main():
 
     #deb2pg/apt% egrep '^Filename: pool' fakedroot/var/lib/apt/lists/deb.debian.org_debian_dists_unstable_main_binary-amd64_Packages | cut -d/ -f 2- > ~/code/fbuilder/unstable.lst
     RE = re.compile('/([^/_]*)_')
+    debs = set()
     with open('data/unstable.lst') as listing:
         for line in listing:
             pkg = RE.search(line).group(1)
             if pkg in needed:
                 needed.remove(pkg)
-                print(mirror + line.strip())
+                parts = line.split('/')
+                debs.add(parts[len(parts) - 1].strip())
+                # print(mirror + line.strip())
 
+    control = defaultdict(dict)
+    for deb in debs:
+        control_gz = subprocess.Popen(['bsdtar', '-Oxf', 'debs/' + deb, 'control.tar.gz'], stdin=subprocess.DEVNULL, stdout=subprocess.PIPE)
+        tar = tarfile.open(fileobj=control_gz.stdout, mode='r|gz')
+        for member in tar:
+            contents = tar.extractfile(member)
+            if contents:
+                control[deb][member.name] = contents.read()
+
+    for deb, content in control.items():
+        for name, data in content.items():
+            print('{} {} {}', deb, name, len(data))
 
 
 if '__main__' == __name__:
